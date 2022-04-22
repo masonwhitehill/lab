2d78bd78c9db49e461c13a9fc8cbab6abacc5310
Mason Whitehill <masonwhitehill@unomaha.edu>
2022-04-22 16:28:47 -0500

    The testing I have so far before I go to work. This is probably not going be done by 10.

--- test_classes.py
+++ test_classes.py
@@ -1,1 +1,13 @@
-print('jk')
+from classes import *
+
+
+class Test:
+    def setup_method(self):
+        self.tv = Television()
+
+    def teardown_method(self):
+        del self.tv
+
+    def test_init(self):
+        assert self.tv.__str__() == 'TV status: Is on = False, Channel = 0, Volume = 0'
+
